576594526b9b6989b3a41e2a6005fef9946c7c07
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-07 18:59:35 -0500

    Closing tags on pages

--- client/src/components/pages/Cardspec.js
+++ client/src/components/pages/Cardspec.js
@@ -6,11 +6,12 @@ export default function Cardspec() {
       <div class="card">
         <h2>({title})</h2>
         <h3>({issue}), ({volume})</h3>
-        <img src="({cover})})" alt="Comic Cover" style="width:100%">
+        <img src="({cover})})" alt="Comic Cover" style="width:100%"/>
         <div class="container">
-        <h>Author: ({author})</h3>
-        <h>Artist: ({artist})</h3>
-        <h>Price: ({price})</h3>
+        <h3>Author: ({author})</h3>
+        <h3>Artist: ({artist})</h3>
+        <h3>Price: ({price})</h3>
+        </div>
       </div>
     </div>
   );
