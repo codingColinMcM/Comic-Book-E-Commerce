93ae29f7a7dd5154742fb655c201d44607567620
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-07 19:07:16 -0500

    Closed tag in cardgen js page

--- client/src/components/pages/Cardgen.js
+++ client/src/components/pages/Cardgen.js
@@ -4,10 +4,11 @@ export default function Cardgen() {
   return (
     <div>
       <div class="card">
-        <img src="({cover})})" alt="Comic Cover" style="width:100%">
+        <img src="({cover})})" alt="Comic Cover" style="width:100%"/>
         <div class="container">
-        <h3>({title}), ({issue}), ({volume})</h3>
-        <h3>({price})</h3>
+          <h3>({title}), ({issue}), ({volume})</h3>
+          <h3>({price})</h3>
+        </div>
       </div>
     </div>
   );
