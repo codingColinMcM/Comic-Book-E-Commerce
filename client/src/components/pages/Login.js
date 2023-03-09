f7b303cef2f1d1aacdbd858aa086d720aab0985d
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-09 10:15:39 -0500

    Changed login js

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -2,27 +2,28 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div>
+    <div className="container text-center m-5-auto">
       <h2>Login Form</h2>
-      <div>
-        <form action="/action_page.php" method="post"/>
-          <div class="container">
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required/>
-    
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required/>
-            
+      <h3>Resume Your Hero Journey</h3>
+            <form>
+                <p>
+                    <label>Email address</label><br/>
+                    <input type="email" name="email" required />
+                </p>
+                <p>
+                    <label>Password</label><br/>
+                    <input type="password" name="password" required />
+                </p>
             <button type="submit">Login</button>
+            </form>
             <label>
             <input type="checkbox" checked="checked" name="remember"/> Remember me
             </label>
-          </div>
-    
           {/* <div class="container" style="background-color:#f1f1f1">
             <span class="psw">Forgot <a href="#">password?</a></span>
-          </div> */}
-      </div>
-    </div>
+          </div> */}  
+          </div>
+    
+          
   );
 }
