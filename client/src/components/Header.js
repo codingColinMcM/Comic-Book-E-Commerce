e0a3d78a547ae0d2713cb29b426f008199c699e7
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-09 09:28:42 -0500

    Finally got logo working on header but need to switch to correct logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image'
+// import Image from 'react-bootstrap/Image'
 import '../styles/Header.css';
+import logo from './logo.gif';
 
 function Header() {
   return (
     <header className="header">
       <h1>Buy, sell, exchange comics!</h1>
-      {/* <img src="../client/public/colin2.gif" alt="Business logo" width="200" height="200"></img> */}
-      <Image src="./pages/logo.gif" fluid></Image>
+      <img src={logo} alt="Business logo" width="200" height="200"></img>
+      {/* <Image src="./logo.gif" fluid></Image> */}
       <input type="text" placeholder="Search.."></input>
     </header>
   );
