1c44f318b379aee63cbca64121c89f506dd53d02
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-09 09:10:27 -0500

    Updated imports for navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
 import "../styles/Navbar.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 // function Navbar () {
 //   return (
