31ab9e059266cff3cf26364acc33119aff89334e
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-09 09:25:23 -0500

    finally got logo to work on about js

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from './logo.gif';
 
 export default function About() {
   return (
-    <div>
+    <div className="container">
+      <img src={logo} alt="logo" height="200" width="200"></img>
       <h1>About Us</h1>
       <p>
         We created this app for the purpose of offering a location for people to buy, sell, exchange comic books.
