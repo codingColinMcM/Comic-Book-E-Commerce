c90738479c99e18f23240fe3d49d50d843224289
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-09 04:05:32 -0500

    Worked on trying to get App routes fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,65 @@
 import React from 'react';
 // import { Route, Routes } from "react-router-dom";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Navbarr from './components/Navbar';
 import Header from './components/Header';
-// import Home from './components/pages/Home';
+import Home from './components/pages/Home';
 // import Comic from './components/pages/Comic';
-// import Dashboard from './components/pages/Dashboard';
-// import About from './components/pages/About';
+import Dashboard from './components/pages/Dashboard';
+import About from './components/pages/About';
 import Cardgen from './components/pages/Cardgen'
 import Cardspec from './components/pages/Cardspec'
+import Signup from './components/pages/Signup'
+import Login from './components/pages/Login'
+import Sell from './components/pages/Sell'
+// import Comic from './components/pages/Comic'
 
 
 function App() {
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home
+      break
+    case "/dashboard":
+      Component = Dashboard
+      break
+    case "/about":
+      Component = About
+      break
+    case "/sell":
+        Component = Sell
+        break
+    case "/login":
+        Component = Login
+        break
+    case "/signup":
+        Component = Signup
+        break
+    case "/dashboard":
+          Component = Dashboard
+          break
+    case "/cardgen":
+            Component = Cardgen
+            break
+    case "/cardspec":
+            Component = Cardspec
+            break
+  }
       return (
         <>
           <Header />
-          <Navbarr />
-          {/* <Home />
-          <Comic />
-          <About /> */}
-          <Cardgen />
-          <Cardspec />
-          {/* <Dashboard />
-          <Discount />
-          <Login />
-          <New />
-          <Sell />
-          <Signup /> */}
-          <div className="container">
-            {/* <Routes>
+          <Navbarr /> 
+          <Component />         
+          {/* <div className="container">
+            <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/comic" element={<Comic />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/about" element={<About />} />
-            </Routes> */}
-            </div>
+            </Routes>
+            </div> */}
     </>
   );
 }
--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -2,8 +2,51 @@ import React from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
-      
-    </div>
+    <>
+      {/* <div class="container bootstrap snippets bootdey">
+        <h1 class="text-primary">Edit Profile</h1>
+        <hr>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="text-center">
+              <img src="placeholder.extension" class="avatar img-circle img-thumbnail" alt="avatar"/>
+              <h6>Upload a different photo...</h6>
+              <input type="file" class="form-control"/>
+            </div>
+          </div>
+          <div class="col-md-9 personal-info">
+            <h3>Personal info</h3>
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label class="col-lg-3 control-label">First name:</label>
+                <div class="col-lg-8">
+                  <input class="form-control" type="text" value="Insert Injected Data"/>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-lg-3 control-label">Last name:</label>
+                <div class="col-lg-8">
+                  <input class="form-control" type="text" value="Insert Injected Data"/>
+                </div>
+              </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Company:</label>
+            <div class="col-lg-8">
+              <input class="form-control" type="text" value=""/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Email:</label>
+            <div class="col-lg-8">
+              <input class="form-control" type="text" value="janesemail@gmail.com"/>
+            </div>
+          </div>
+        </form>
+      </div>
+  </div>
+ </hr> 
+</div> */}
+
+</>  
   );
 }
