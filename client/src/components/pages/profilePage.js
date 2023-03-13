73799a65ab2dd078d5729f7ababcfea400bae481
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-03-13 01:58:21 -0400

    added feature

--- client/src/components/pages/profilePage.js
+++ client/src/components/pages/profilePage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React from 'react'
 import Group from './Group';
 import ColorBox from './ColorBox';
 import { months, calcButtonTextColor } from '../tools';
