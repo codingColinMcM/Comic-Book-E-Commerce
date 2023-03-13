ae260ac42a5191e22d7ba2f9f97d1b10f48d0398
galegro2022 <112990654+galegro2022@users.noreply.github.com>
2023-03-12 20:01:00 -0400

    new push

--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes);
+//app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client'));
+  res.sendFile(path.join(__dirname, '../client/build'));
 });
 
 
